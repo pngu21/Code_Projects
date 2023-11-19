7179e8bb528a9f59ecbf895af2c22b1eefe0e891
Phila Ngubentombi <philangubentombi@gmail.com>
2023-11-19 15:58:57 +0200

    Project commit

---
+++ react-project/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var hW1 = React.createElement('h3', null, 'Hello World')
+var hW2 = React.createElement('h3', null, 'Nice to Meet You')
+
+var div1 = React.createElement('div', null, hW1, hW2);
+
+ReactDOM.render(
+    div1,
+    document.getElementById('app')
+)
