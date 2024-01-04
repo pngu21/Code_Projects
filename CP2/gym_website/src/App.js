fa8a98b9a720c00a0952a2603e83beac6ce6b258
Phila Ngubentombi <philangubentombi@gmail.com>
2024-01-05 00:55:38 +0200

    Header-and-Features

--- CP2/gym_website/src/App.js
+++ CP2/gym_website/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './Componenets/Navbar';
+import Header from './Componenets/Header';
+import Feature from './Componenets/Feature';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Header/>
+      <Feature/>
     </div>
   );
 }
