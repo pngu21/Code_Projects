564dd995d9a85acd2fa03b04231163f9ed6022b6
Phila Ngubentombi <philangubentombi@gmail.com>
2024-09-26 16:50:59 +0200

    Project Change

--- CP5/portfolio_resume/src/Components/Projects.jsx
+++ CP5/portfolio_resume/src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../index.css';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 // import styled from 'styled-components';
 // const ProjectsContainer = styled.section`
 //   padding: 20px;
@@ -8,10 +9,52 @@ import '../index.css';
 const Projects = () => (
   <section className="projects-container">
     <h2>Projects</h2>
-    <ul>
-      <li>Project X - Description of project X</li>
-      <li>Project Y - Description of project Y</li>
-    </ul>
+
+  <div className="cards-container">
+    <div className="card">
+      <Link to="/pro1">
+        <div className="card-content">
+          <h3>Project 1</h3>
+        </div>
+      </Link>
+    </div>
+    <div className="card">
+      <Link to="/pro2">
+        <div className="card-content">
+          <h3>Project 2</h3>
+        </div>
+      </Link>
+    </div>
+    <div className="card">
+      <Link to="/pro3">
+        <div className="card-content">
+          <h3>Project 3</h3>
+        </div>
+      </Link>
+    </div>
+    <div className="card">
+      <Link to="/pro4">
+        <div className="card-content">
+          <h3>Project 4</h3>
+        </div>
+      </Link>
+    </div>
+    <div className="card">
+      <Link to="/pro5">
+        <div className="card-content">
+          <h3>Project 5</h3>
+        </div>
+      </Link>
+    </div>
+    <div className="card">
+      <Link to="/pro6">
+        <div className="card-content">
+          <h3>Project 6</h3>
+        </div>
+      </Link>
+    </div>
+  </div>
+
   </section>
 );
 
