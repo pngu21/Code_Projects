8ad94e6d60a8afa0f75f5d7dd611c6b1cfe50a40
Phila Ngubentombi <philangubentombi@gmail.com>
2024-10-22 20:40:50 +0200

    Contact me form and added images

--- CP5/portfolio_resume/src/Components/Contact.jsx
+++ CP5/portfolio_resume/src/Components/Contact.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
+import { useFormData } from 'herotofu-react';
 import '../index.css';
-// import styled from 'styled-components';
-
-// const ContactContainer = styled.section`
-//   padding: 20px;
-//   text-align: center;
-// `;
+import image4 from '../images/test4.jpg';
 
 const Contact = () => (
-  <section className="contact-container">
-    <h2>Contact</h2>
-    <p>Email: philangubentombi@gmail.com</p>
-    <p>Phone: +27630249334</p>
-    <p>GIT: https://github.com/pngu21</p>
-  </section>
+  <div className="contact-container">
+    <div className="contact-form-container">
+    <h2>Contact Me</h2>
+      <form className="contact-form">
+        <label htmlFor="name">Name:</label>
+        <input id="firstname" name="firstname" type="text" placeholder=" Enter Your Name" required/>
+        
+        <label htmlFor="surname">Surname:</label>
+        <input id="surname" name="surname" type="text" placeholder=" Enter Your Surname" required/>
+        
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="text" placeholder=" Enter Your Email" required/>
+        
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" rows="5" cols="60" required></textarea>
+        
+        <button vaule="Submit" type="submit">Send</button>
+      </form>
+    </div>
+    <div className="contact-image-container">
+      <div className="image-overlay"></div>
+      <img 
+        src={image4} alt="Portrait"
+      />
+    </div>
+  </div>
 );
 
 export default Contact;
