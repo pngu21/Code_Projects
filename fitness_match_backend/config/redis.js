93c4b8af235ba927461666e431a0c8806cc9dcef
Phila Ngubentombi <philangubentombi@gmail.com>
2025-03-17 22:00:32 +0200

    removed app

--- fitness_match_backend/config/redis.js
+++
@@ -1,7 +0,0 @@
-const redis = require("redis");
-const client = redis.createClient();
-
-client.on("error", (err) => console.error("Redis Error:", err));
-client.on("connect", () => console.log("Redis Connected"));
-
-module.exports = client;
--- fitness_match_backend/routes/match.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { matchUser } = require("../controllers/matchController");
-const router = express.Router();
-
-router.post("/match", matchUser);
-
-module.exports = router;
